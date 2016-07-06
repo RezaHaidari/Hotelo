fe7afc5ef3372a2b712016c443d3a0cae32d80f6
Reza Haidari <shoosh2007@gmail.com>
2016-07-06 16:07:27 +0430

    firs commit.

--- app/User.php
+++ app/User.php
@@ -23,7 +23,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
+    
     public function isHotelManager(){
         return false;
     }
