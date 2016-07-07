a45ad1785557965f21ad5edd7bb26f9fa9c68a22
Reza Haidari <shoosh2007@gmail.com>
2016-07-07 17:42:36 +0430

    Add Project

---
+++ public/themes/newTheme/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>New Theme</title>
+    <link rel="stylesheet" href="{{theme('css/style.css')}}" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+
+    <h2> this is new Theme</h2>
+
+
+
+
+  </body>
+</html>
