0d7d10a9854cae559054f79c6184566c1b71befc
Reza Haidari <shoosh2007@gmail.com>
2016-07-20 05:02:18 +0430

    add another models, and update project

---
+++ public/themes/hotelo/views/layouts/header_front.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en" dir="rtl">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="{{theme('frontend/css/reset.css')}}">
+    <link rel="stylesheet" href="{{theme('frontend/css/style.css')}}">
+    <link rel="stylesheet" href="{{theme('frontend/js/modernizr.js')}}">
+
+    <title>{{$title}}</title>
+</head>
+
+<head>
